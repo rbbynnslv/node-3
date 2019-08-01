9d5e0b8a35786b18f9efc03af83fbdc7c200e5df
Robby Ann Eslava <50604868+rbbynnslv@users.noreply.github.com>
2019-08-01 04:26:29 -0700

    Update comments.js

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,84 +1,44 @@
-const verify = require('./verify');
-
 function create(req, res) {
-    const auth = verify.verifyUser(req, res);
-    if (auth) {
-        const db = req.app.get('db');
-        const { userId, postId, comment } = req.body
+    const db = req.app.get('db');
+  
+    const { userId, postId, comment } = req.body;
+  
+    db.comments
+    .insert(
+        {
+        userId,
+        postId,
+        comment
+        },
+    )
+    .then(user => res.status(201).json(user))
+    .catch(err => {
+        console.error(err);
+  });
+  }
 
-        db.comments
-            .insert({
-                userId,
-                postId,
-                comment,
-            })
-            .then(comment => res.status(201).json(comment))
-            .catch(err => {
-                console.error(err);
-            });
-    }
-    else res.status(401).end();
+  function editComment(req, res) {
+    const db = req.app.get('db');
+
+    const { comment } = req.body;
+
+    db.comments
+        .update({
+            id: req.params.id,
+            userId: req.params.userId
+        }, {
+            comment: comment,
+        })
+        .then(user => res.status(201).json(user))
+        .catch(err => {
+            console.error(err);
+            res.status(500).end();
+        });
+
 }
 
-function updateComment(req, res) {
-    const auth = verify.verifyUser(req, res);
-    if (auth) {
-        const db = req.app.get('db');
-        const { id } = req.params;
-        const { comment } = req.body;
-        db.comments
-            .save({ id, comment })
-            .then(comment => res.status(200).json(comment))
-            .catch(err => {
-                console.error(err);
-                res.status(500).end();
-            });
-    }
-    else res.status(401).end();
-}
 
 module.exports = {
-    create,
-    updateComment
-}
-function create(req, res) {
-    const auth = verify.verifyUser(req, res);
-    if (auth) {
-        const db = req.app.get('db');
-        const { userId, postId, comment } = req.body
-
-        db.comments
-            .insert({
-                userId,
-                postId,
-                comment,
-            })
-            .then(comment => res.status(201).json(comment))
-            .catch(err => {
-                console.error(err);
-            });
-    }
-    else res.status(401).end();
-}
-
-function updateComment(req, res) {
-    const auth = verify.verifyUser(req, res);
-    if (auth) {
-        const db = req.app.get('db');
-        const { id } = req.params;
-        const { comment } = req.body;
-        db.comments
-            .save({ id, comment })
-            .then(comment => res.status(200).json(comment))
-            .catch(err => {
-                console.error(err);
-                res.status(500).end();
-            });
-    }
-    else res.status(401).end();
-}
-
-module.exports = {
-    create,
-    updateComment
-}
+    create, 
+    editComment,
+};
