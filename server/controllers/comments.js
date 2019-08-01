b8ea45eba99d5982b17f4483edfc4b93a6ba518e
Robby Ann Eslava <50604868+rbbynnslv@users.noreply.github.com>
2019-08-01 04:21:58 -0700

    Update comments.js

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,3 +1,84 @@
-module.exports ={
-    
+const verify = require('./verify');
+
+function create(req, res) {
+    const auth = verify.verifyUser(req, res);
+    if (auth) {
+        const db = req.app.get('db');
+        const { userId, postId, comment } = req.body
+
+        db.comments
+            .insert({
+                userId,
+                postId,
+                comment,
+            })
+            .then(comment => res.status(201).json(comment))
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    else res.status(401).end();
+}
+
+function updateComment(req, res) {
+    const auth = verify.verifyUser(req, res);
+    if (auth) {
+        const db = req.app.get('db');
+        const { id } = req.params;
+        const { comment } = req.body;
+        db.comments
+            .save({ id, comment })
+            .then(comment => res.status(200).json(comment))
+            .catch(err => {
+                console.error(err);
+                res.status(500).end();
+            });
+    }
+    else res.status(401).end();
+}
+
+module.exports = {
+    create,
+    updateComment
+}
+function create(req, res) {
+    const auth = verify.verifyUser(req, res);
+    if (auth) {
+        const db = req.app.get('db');
+        const { userId, postId, comment } = req.body
+
+        db.comments
+            .insert({
+                userId,
+                postId,
+                comment,
+            })
+            .then(comment => res.status(201).json(comment))
+            .catch(err => {
+                console.error(err);
+            });
+    }
+    else res.status(401).end();
+}
+
+function updateComment(req, res) {
+    const auth = verify.verifyUser(req, res);
+    if (auth) {
+        const db = req.app.get('db');
+        const { id } = req.params;
+        const { comment } = req.body;
+        db.comments
+            .save({ id, comment })
+            .then(comment => res.status(200).json(comment))
+            .catch(err => {
+                console.error(err);
+                res.status(500).end();
+            });
+    }
+    else res.status(401).end();
+}
+
+module.exports = {
+    create,
+    updateComment
 }
