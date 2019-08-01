97ace228232396a04b8790d2e25a4daf1a7e1083
dianageromo <diana.geromo@boom.camp>
2019-08-01 16:25:14 +0800

    Node-3-Submission-Updated

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,70 +1,118 @@
-module.exports = {
-    create: function (req, res) {
-        const db = req.app.get('db');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const secret = require('../../secret');
 
-        const { email, password } = req.body;
+function register(req, res) {
+  const db = req.app.get('db');
+  const { username, email, password } = req.body;
 
-        db.users
-            .insert(
-                {
-                    email,
-                    password,
-                    user_profiles: [
-                        {
-                            userId: undefined,
-                            about: null,
-                            thumbnail: null,
-                        },
-                    ],
-                },
-                {
-                    deepInsert: true,
-                }
-            )
-            .save(
-                {
-                    email,
-                    password,
-                }
-            )
-            .then(user => res.status(201).json(user))
-            .catch(err => {
-                console.log(err);
-            })
+  argon2
+    .hash(password)
+    .then(hash => {
+      return db.users.insert(
+        {
+          username,
+          email,
+          password: hash,
+        },
+        {
+          fields: ['id', 'username', 'email'],
+        }
+      );
+    })
+    .then(user => {
+      const token = jwt.sign({ userId: user.id }, secret); // adding token generation
+      res.status(201).json({ ...user, token });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).end();
+    });
+}
+
+function create(req, res) {
+    const db = req.app.get('db');
+  
+    const { email, password } = req.body;
+  
+    db.users // heres the new stuff, using massive to actually query the database.
+      .save({
+        email,
+        password,
+      })
+      .then(user => res.status(201).json(user)) // returns a promise so we need to use .then
+      .catch(err => {
+        console.error(err); // if something happens we handle the error as well.
+        res.status(500).end();
+      });
+  }
+
+  db.users
+  .insert(
+    {
+      email,
+      password,
+      user_profiles: [
+        // this is what is specifying the object
+        // to insert into the related 'user_profiles' table
+        {
+          userId: undefined,
+          about: null,
+          thumbnail: null,
+        },
+      ],
     },
-    list: function (req, res) {
-        const db = req.app.get('db');
-
-        db.users
-            .find()
-            .then(users => res.status(200).json(users))
-            .catch(err => {
-                console.error(err);
-                res.status(500).end();
-            });
-    },
-    getById: function (req, res) {
-        const db = req.app.get('db');
-
-        db.users
-            .findOne(req.params.id)
-            .then(user => res.status(200).json(user))
-            .catch(err => {
-                console.error(err);
-                res.status(500).end();
-            });
-    },
-    getProfile: function (req, res) {
-        const db = req.app.get('db');
-
-        db.user_profiles
-            .findOne({
-                userId: req.params.id,
-            })
-            .then(profile => res.status(200).json(profile))
-            .catch(err => {
-                console.error(err);
-                res.status(500).end();
-            });
+    {
+      deepInsert: true, // this option here tells massive to create the related object
     }
+  )
+  .then(user => res.status(201).json(user))
+  .catch(err => {
+    console.error(err);
+  });
+
+  function list(req, res) {
+    const db = req.app.get('db');
+  
+    db.users
+      .find()
+      .then(users => res.status(200).json(users))
+      .catch(err => {
+        console.error(err);
+        res.status(500).end();
+      });
+  }
+
+  function getById(req, res) {
+    const db = req.app.get('db');
+  
+    db.users
+      .findOne(req.params.id)
+      .then(user => res.status(200).json(user))
+      .catch(err => {
+        console.error(err);
+        res.status(500).end();
+      });
+  }
+
+  function getProfile(req, res) {
+    const db = req.app.get('db');
+  
+    db.user_profiles
+      .findOne({
+        userId: req.params.id,
+      })
+      .then(profile => res.status(200).json(profile))
+      .catch(err => {
+        console.error(err);
+        res.status(500).end();
+      });
+  }
+  
+module.exports = {
+   register,
+   create,
+   list,
+   getById,
+   getProfile,
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const massive = require('massive')
+const jwt = require('jsonwebtoken');
+const secret = require('../secret');
 
 const users = require('./controllers/users.js')
+const users = require('./controllers/login.js')
 
 massive({
     host: 'localhost',
@@ -16,10 +19,29 @@ massive({
 
     app.use(express.json());
 
+    app.post('/api/register', users.register);
+    app.get('/api/protected/data',
+    function (req, res) {
+        if (!req.headers.authorization) {
+            return res.status(401).end();
+        }
+
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, secret); // will throw an Error when token is invalid!!!
+            res.status(200).json({ data: 'here is the protected data' });
+        } catch (err) {
+            console.error(err);
+            res.status(401).end();
+        }
+    });
+
     app.post('api/users', users.create);
     app.get('api/users', users.list);
     app.get('api/users/:id', users.getById);
     app.get('api/users/:id/profile', users.getProfile);
+
+    app.post('/api/login', login.login);
 
     app.post('api/posts', posts.create);
 
